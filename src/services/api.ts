f184583079185c93d2c234224aee9576a27f5268
‘Solomon <solomonojigbo@gmail.com>
2025-05-07 08:53:00 +0100

    Refactor api services API_URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError} from 'axios';
 
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://shortlink-server.vercel.app/';
+const API_URL = import.meta.env.VITE_API_URL || 'https://shortlink-server.vercel.app';
 
 const apiClient = axios.create({
   baseURL: API_URL,
